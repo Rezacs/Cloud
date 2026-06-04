34034d70d996c438ed67b8e959fa1d7eb37ccf9c
Reza <ralmassizx@gmail.com>
2026-06-04 03:29:18 +0200

    added also script to run Mega

---
+++ run_tests_mega.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+bash ~/Cloud/run_one_dataset.sh \
+  mega \
+  "/input/gutenberg-mega" \
+  "hdfs:///input/gutenberg-mega" \
+  "/var/backups/hadoop/backup_before_reinstall/AllDatasets/Mega"
