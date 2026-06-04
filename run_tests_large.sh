389e924476e33eeae09e786ba83d6479d0d9e0d8
Reza <ralmassizx@gmail.com>
2026-06-05 01:15:43 +0200

    deleting unnecessary files

--- run_tests_large.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-bash ~/Cloud/run_one_dataset.sh \
-  large \
-  "/input/gutenberg-large" \
-  "hdfs:///input/gutenberg-large" \
-  "/var/backups/hadoop/backup_before_reinstall/AllDatasets/large"
--- run_tests_medium.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-bash ~/Cloud/run_one_dataset.sh \
-  medium \
-  "/input/gutenberg-medium" \
-  "hdfs:///input/gutenberg-medium" \
-  "/var/backups/hadoop/backup_before_reinstall/AllDatasets/medium"
