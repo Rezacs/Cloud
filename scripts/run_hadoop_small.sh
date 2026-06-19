01232edaf57210606368fb1924030b633a00af6e
Reza <ralmassizx@gmail.com>
2026-06-19 02:10:06 +0200

    new changes on Windows

--- scripts/run_hadoop_small.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-hdfs dfs -rm -r -f /output/hadoop-index-small
-
-/usr/bin/time -v hadoop jar ~/Cloud/hadoop-java/target/hadoop-inverted-index-1.0.jar \
-it.unipi.cloud.InvertedIndex \
-/input/full_dataset/* \
-/output/hadoop-index-small
--- scripts/run_spark_small.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-hdfs dfs -rm -r -f /output/spark-index-small
-
-/usr/bin/time -v spark-submit \
-~/Cloud/spark-python/inverted_index_spark.py \
-hdfs:///input/full_dataset/* \
-hdfs:///output/spark-index-small
